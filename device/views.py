66b4718a682bfa41329f773f7bb8195032d3cf6a
root <root@capistrano.example.com>
2013-07-17 14:36:50 +0800

    用户登录

--- device/views.py
+++ device/views.py
@@ -1,6 +1,7 @@
 #!coding:utf-8
 from django.shortcuts import render_to_response, redirect, RequestContext, render
 from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
 from django import forms
 from device.models import Device
 import datetime
@@ -14,68 +15,55 @@ class DeviceForm(forms.ModelForm):
 #	if Device.objects.filter(nei_ip = nei_ip.strip()):
 #	    raise forms.ValidationError("nei_ip repeat!!")
 
+@login_required(login_url='/login/')
 def manage(request):
-    user = request.user
-    if user.is_authenticated():
-        if request.method == "POST" :
-            df = DeviceForm(request.POST)
-	    if df.is_valid():
-		nei_ip		= df.cleaned_data['nei_ip']
-		wai_ip		= df.cleaned_data['wai_ip']
-		hostname        = df.cleaned_data['hostname']
-		serial_number   = df.cleaned_data['serial_number']
-		buy_date        = df.cleaned_data['buy_date']
-		inventar_Nummer = df.cleaned_data['inventar_Nummer']
-		manufacturer    = df.cleaned_data['manufacturer']
-		type            = df.cleaned_data['type']
-		monitor         = df.cleaned_data['monitor']
-		location        = df.cleaned_data['location']
-		principal       = df.cleaned_data['principal']
-		administrator   = df.cleaned_data['administrator']
-		purpose         = df.cleaned_data['purpose']
-		status		= df.cleaned_data['status']
-		remarks         = df.cleaned_data['remarks']
-		os              = df.cleaned_data['os']
-		partion         = df.cleaned_data['partion']
-		cpu             = df.cleaned_data['cpu']
-		memory          = df.cleaned_data['memory']
-		disk            = df.cleaned_data['disk']
-		disk_controller = df.cleaned_data['disk_controller']
-		protocol        = df.cleaned_data['protocol']
-		port            = df.cleaned_data['port']
-		username        = df.cleaned_data['username']
-		password        = df.cleaned_data['password']
-#		buy_date = "-".join(get_date.split('/')[::-1])
-                Device.objects.create(nei_ip=nei_ip, wai_ip=wai_ip, hostname=hostname, serial_number=serial_number, buy_date=buy_date, inventar_Nummer=inventar_Nummer, manufacturer=manufacturer, type=type, monitor=monitor, location=location, principal=principal, administrator=administrator, purpose=purpose, status=status, remarks=remarks, os=os, partion=partion, cpu=cpu, memory=memory, disk=disk, disk_controller=disk_controller, protocol=protocol, port=port, username=username, password=password)
-		return HttpResponseRedirect("/device/list/")
-	else:
-	    df=DeviceForm()
-	    return render_to_response('manage_device.html',{'df':df},context_instance=RequestContext(request))
+    if request.method == "POST" :
+        df = DeviceForm(request.POST)
+        if df.is_valid():
+	    nei_ip		= df.cleaned_data['nei_ip']
+    	    wai_ip		= df.cleaned_data['wai_ip']
+    	    hostname        = df.cleaned_data['hostname']
+    	    serial_number   = df.cleaned_data['serial_number']
+    	    buy_date        = df.cleaned_data['buy_date']
+    	    inventar_Nummer = df.cleaned_data['inventar_Nummer']
+    	    manufacturer    = df.cleaned_data['manufacturer']
+    	    type            = df.cleaned_data['type']
+    	    monitor         = df.cleaned_data['monitor']
+    	    location        = df.cleaned_data['location']
+    	    principal       = df.cleaned_data['principal']
+    	    administrator   = df.cleaned_data['administrator']
+    	    purpose         = df.cleaned_data['purpose']
+    	    status		= df.cleaned_data['status']
+    	    remarks         = df.cleaned_data['remarks']
+    	    os              = df.cleaned_data['os']
+    	    partion         = df.cleaned_data['partion']
+    	    cpu             = df.cleaned_data['cpu']
+    	    memory          = df.cleaned_data['memory']
+    	    disk            = df.cleaned_data['disk']
+    	    disk_controller = df.cleaned_data['disk_controller']
+    	    protocol        = df.cleaned_data['protocol']
+    	    port            = df.cleaned_data['port']
+    	    username        = df.cleaned_data['username']
+    	    password        = df.cleaned_data['password']
+            Device.objects.create(nei_ip=nei_ip, wai_ip=wai_ip, hostname=hostname, serial_number=serial_number, buy_date=buy_date, inventar_Nummer=inventar_Nummer, manufacturer=manufacturer, type=type, monitor=monitor, location=location, principal=principal, administrator=administrator, purpose=purpose, status=status, remarks=remarks, os=os, partion=partion, cpu=cpu, memory=memory, disk=disk, disk_controller=disk_controller, protocol=protocol, port=port, username=username, password=password)
+    	return HttpResponseRedirect("/device/list/")
     else:
-	return HttpResponseRedirect("/login/")
+        df=DeviceForm()
+        return render_to_response('manage_device.html',{'df':df},context_instance=RequestContext(request))
 
+@login_required(login_url='/login/')
 def list(request):
-    user = request.user
-    if user.is_authenticated():
-	devices = Device.objects.order_by('nei_ip').all()
-        return render_to_response('list_device.html', {'devices':devices}, context_instance=RequestContext(request))
-    else:
-	return HttpResponseRedirect("/login/")
+    devices = Device.objects.order_by('nei_ip').all()
+    return render_to_response('list_device.html', {'devices':devices}, context_instance=RequestContext(request))
 
+@login_required(login_url='/login/')
 def search(request):
-    user = request.user
-    if user.is_authenticated():
-	if request.method == "POST":
-	    search = request.POST.get('search', 'NONE')
-	    devices = Device.objects.order_by('nei_ip').filter(nei_ip__icontains=search)
-	    return render_to_response('list_device.html', {'devices':devices}, context_instance=RequestContext(request))
-    else:
-	return HttpResponseRedirect("/login/")
+    if request.method == "POST":
+        search = request.POST.get('search', 'NONE')
+        devices = Device.objects.order_by('nei_ip').filter(nei_ip__icontains=search)
+        return render_to_response('list_device.html', {'devices':devices}, context_instance=RequestContext(request))
 
+@login_required(login_url='/login/')
 def every(request,nei_ip):
-    user = request.user
-    if user.is_authenticated():
-        device = Device.objects.get(nei_ip=nei_ip.strip())
-	return render_to_response('info_device.html', {'device':device}, context_instance=RequestContext(request))
-    else:
-        return HttpResponseRedirect("/login/")
+    device = Device.objects.get(nei_ip=nei_ip.strip())
+    return render_to_response('info_device.html', {'device':device}, context_instance=RequestContext(request))
--- project/views.py
+++ project/views.py
@@ -1,6 +1,7 @@
 #!coding:utf-8
 from django.shortcuts import render_to_response, redirect, RequestContext, render
 from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
 from django import forms
 from device.models import Device
 from project.models import Project
@@ -10,50 +11,37 @@ class ProjectForm(forms.ModelForm):
     class Meta:
 	model = Project
 
+@login_required(login_url='/login/')
 def manage(request):
-    user = request.user
-    if user.is_authenticated():
-        if request.method == "POST" :
-            pf = ProjectForm(request.POST)
-	    if pf.is_valid():
-		name	 = pf.cleaned_data['name']
-		web	 = pf.cleaned_data['web']
-		document = pf.cleaned_data['document']
-                p = Project.objects.create(name=name, web=web, document=document)
-
-		device   = pf.cleaned_data['device']
-		for entry in pf.cleaned_data['device']:
-		    p.device.add(entry)
-		return HttpResponseRedirect("/project/list/")
-	else:
-	    devices=Device.objects.all()
-	    pf=ProjectForm()
-	    return render_to_response('manage_project.html',{'pf':pf,'devices':devices},context_instance=RequestContext(request))
+    if request.method == "POST" :
+        pf = ProjectForm(request.POST)
+        if pf.is_valid():
+    	    name = pf.cleaned_data['name']
+    	    web	= pf.cleaned_data['web']
+	    document = pf.cleaned_data['document']
+            p = Project.objects.create(name=name, web=web, document=document)
+    	    device = pf.cleaned_data['device']
+    	    for entry in pf.cleaned_data['device']:
+    	         p.device.add(entry)
+    	    return HttpResponseRedirect("/project/list/")
     else:
-	return HttpResponseRedirect("/login/")
+        devices=Device.objects.all()
+        pf=ProjectForm()
+        return render_to_response('manage_project.html',{'pf':pf,'devices':devices},context_instance=RequestContext(request))
 
+@login_required(login_url='/login/')
 def list(request):
-    user = request.user
-    if user.is_authenticated():
-	projects = Project.objects.order_by('name').all()
-        return render_to_response('list_project.html', {'projects':projects}, context_instance=RequestContext(request))
-    else:
-	return HttpResponseRedirect("/login/")
+    projects = Project.objects.order_by('name').all()
+    return render_to_response('list_project.html', {'projects':projects}, context_instance=RequestContext(request))
 
+@login_required(login_url='/login/')
 def search(request):
-    user = request.user
-    if user.is_authenticated():
-	search = request.POST.get('search', 'noproject')
-	projects = Project.objects.order_by('name').filter(name__icontains=search)
-        return render_to_response('list_project.html', {'projects':projects}, context_instance=RequestContext(request))
-    else:
-	return HttpResponseRedirect("/login/")
+    search = request.POST.get('search', 'noproject')
+    projects = Project.objects.order_by('name').filter(name__icontains=search)
+    return render_to_response('list_project.html', {'projects':projects}, context_instance=RequestContext(request))
 
+@login_required(login_url='/login/')
 def every(request, proname):
-    user = request.user
-    if user.is_authenticated():
-	project = Project.objects.get(name=proname)
-	devices = project.device.all()
-	return render_to_response('info_project.html', {'project':project, 'devices':devices}, context_instance=RequestContext(request))
-    else:
-	return HttpResponseRedirect('/login/')
+    project = Project.objects.get(name=proname)
+    devices = project.device.all()
+    return render_to_response('info_project.html', {'project':project, 'devices':devices}, context_instance=RequestContext(request))
